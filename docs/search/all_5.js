6b1710bfc3e6092c94624b8e8d9c3f327c36f2cb
nobodyxu <Jiahao_XU@outlook.com>
2020-11-21 19:59:39 +1100

    Add docs/ built from Makefile

---
+++ docs/search/all_5.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['set_5fexception_11',['set_exception',['../classValOrErr__t.html#a7f50f5ca56088733bac36ba912896e61',1,'ValOrErr_t']]],
+  ['set_5freturn_5fvalue_12',['set_return_value',['../classValOrErr__t.html#a3e6bdd2591a48e3dd4ecab6e93160c3d',1,'ValOrErr_t']]]
+];
---
+++ docs/search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['catch_26',['Catch',['../classValOrErr__t.html#a3dd54163f7cab50cdcc99a08e76487dc',1,'ValOrErr_t']]]
+];
